3650637264299b71a30bff82806305eda196acb6
Кристина <ultimaaathule@mail.ru>
2022-12-13 14:06:42 +0300

    все задачи

---
+++ Дз (Семинар 1)/Program.cs
@@ -0,0 +1,53 @@
+﻿//задача 8
+
+Console.WriteLine("Введите число");
+string a = Console.ReadLine();
+int a1 = Convert.ToInt32(a);
+if (a1==5);
+{
+Console.WriteLine($"{2} {4}");
+}
+string b = Console.ReadLine();
+int b1 = Convert.ToInt32(b);
+if (b1==8);
+{
+Console.WriteLine($"{2} {4} {6} {8}");
+}
+
+
+//задача 4
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > a)
+max = b;
+if (c > max)
+max = c;
+Console.WriteLine(max);
+
+// задача 6
+
+Console.WriteLine ("Введите число");
+string a = Console.ReadLine();
+int a1 = Convert.ToInt32(a);
+if(a1==4)
+{
+ Console.WriteLine("Да"); 
+}
+string b = Console.ReadLine();
+int b1 = Convert.ToInt32(a);
+if(a1==-3)
+{
+ Console.WriteLine("Нет"); 
+}
+string c = Console.ReadLine();
+int c1 = Convert.ToInt32(a);
+if(c1==7)
+{
+ Console.WriteLine("Нет"); 
+}
