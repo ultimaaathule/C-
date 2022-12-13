35a46093cc6fdfcc9a92f69c10da25cd40372200
Кристина <ultimaaathule@mail.ru>
2022-12-13 13:58:59 +0300

    решение сразу 3-х задач

---
+++ Задача 4/Program.cs
@@ -0,0 +1,22 @@
+﻿// ЗАДАЧА 6!
+
+Console.WriteLine ("Введите число");
+string a = Console.ReadLine();
+int a1 = Convert.ToInt32(a);
+if(a1==4)
+{
+ Console.WriteLine("Да"); 
+}
+string b = Console.ReadLine();
+int b1 = Convert.ToInt32(a);
+if(a1==-3)
+{
+ Console.WriteLine("Нет"); 
+}
+string c = Console.ReadLine();
+int c1 = Convert.ToInt32(a);
+if(c1==7)
+{
+ Console.WriteLine("Нет"); 
+}
+
