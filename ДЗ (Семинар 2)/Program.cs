5f699893dbccb988b08a3dd13f9c9731067a6f4b
Кристина <ultimaaathule@mail.ru>
2022-12-16 16:21:13 +0300

    Дз семинар 2

---
+++ ДЗ (Семинар 2)/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 15
+Console.WriteLine("ВВедите число  дня недели");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 5)
+Console.WriteLine("Ура выходной!");
+else 
+{
+  Console.WriteLine("Еще нужно потрудиться((("); 
+}
+
+//Задача 13
+Console.WriteLine("ВВедите число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n = n / 10; n > 1000)
+Console.WriteLine("n % 10");
+
+//Задача 10
+Console.WriteLine("ВВедите трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int = result;
+if (n < 100 || n > 999)
+{
+  Console.WriteLine(" ВВедите не техзначное число");
+}
+else
+{
+    result = (n / 10) % 10;
+      Console.WriteLine(result);
+
+}
+
+
+
+
+
+
---
+++ Задача 8/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("ВВедите цифру");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 1)
+Console.WriteLine("Рабочий день");
+else if (n == 2)
+Console.WriteLine("Рабочий день");
+else if (n == 3)
+Console.WriteLine("Рабочий день");
+else if (n == 4)
+Console.WriteLine("Рабочий день");
+else if (n == 5)
+Console.WriteLine("Рабочий день");
+else if (n == 6)
+Console.WriteLine("Ура! Чилл!");
+else if (n == 4)
+Console.WriteLine("Ура! Чилл!");
+
+
+
+
